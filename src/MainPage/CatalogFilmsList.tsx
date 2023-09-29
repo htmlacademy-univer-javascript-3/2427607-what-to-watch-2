4fafe9bbe6df6440aaad9000873a4a1130e393d7
inna24s <inna-voronkova0@mail.ru>
2023-09-29 10:47:15 +0300

    fix key

--- src/MainPage/CatalogFilmsList.tsx
+++ src/MainPage/CatalogFilmsList.tsx
@@ -4,6 +4,6 @@ import {CatalogFilmCardProps} from '../data/types';
 export const CatalogFilmsList = (data: {films: CatalogFilmCardProps[]}) => (
   <div className="catalog__films-list">
     {data.films.map((i) =>
-      <CatalogFilmsCard key={i} image={i.image} title={i.title}/>)}
+      <CatalogFilmsCard key={i.toString()} image={i.image} title={i.title}/>)}
   </div>
 );
