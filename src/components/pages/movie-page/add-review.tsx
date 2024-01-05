3740279948dea2979d862e64eb1e9a7e207ec448
inna24s <inna-voronkova0@mail.ru>
2024-01-05 23:32:54 +0300

    task 1

--- src/components/pages/movie-page/add-review.tsx
+++ src/components/pages/movie-page/add-review.tsx
@@ -1,44 +1,40 @@
 import {Header} from '../../header';
 import {Link} from 'react-router-dom';
 import {ReviewForm} from './review-form';
-import {useAppDispatch, useAppSelector} from '../../../hooks';
-import {fetchFilm} from '../../../store/api-actions';
-import {getFilm} from '../../../store/film-data/selectors';
 import {RequestPending} from '../../pending-request/pending-request';
+import {useSelectedFilm} from '../../../hooks/use-selected-film';
 
 export const AddReview = ()=> {
-  const filmData = useAppSelector(getFilm);
-  const dispatch = useAppDispatch();
-  if (!filmData){
-    dispatch(fetchFilm('6c84c13e-e4e0-4bcc-bdb0-bd25b1ab5d8d'));
-  }
+  const {filmData} = useSelectedFilm({});
   return (
     <RequestPending>
       <section className="film-card film-card--full">
-        <div className="film-card__header">
-          <div className="film-card__bg">
-            <img src={filmData.backgroundImage} alt={filmData.name}/>
+        {filmData && (
+          <div className="film-card__header">
+            <div className="film-card__bg">
+              <img src={filmData.backgroundImage} alt={filmData.name}/>
+            </div>
+
+            <h1 className="visually-hidden">WTW</h1>
+
+            <Header>
+              <nav className="breadcrumbs">
+                <ul className="breadcrumbs__list">
+                  <li className="breadcrumbs__item">
+                    <Link to={`/films/${filmData.id}/`} className="breadcrumbs__link"> {filmData.name} </Link>
+                  </li>
+                  <li className="breadcrumbs__item">
+                    <Link to="#" className="breadcrumbs__link">Add review</Link>
+                  </li>
+                </ul>
+              </nav>
+            </Header>
+
+            <div className="film-card__poster film-card__poster--small">
+              <img src={filmData.posterImage} alt={filmData.name} width="218" height="327"/>
+            </div>
           </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <Header>
-            <nav className="breadcrumbs">
-              <ul className="breadcrumbs__list">
-                <li className="breadcrumbs__item">
-                  <Link to={`/films/${filmData.id}/`} className="breadcrumbs__link"> {filmData.name} </Link>
-                </li>
-                <li className="breadcrumbs__item">
-                  <Link to="#" className="breadcrumbs__link">Add review</Link>
-                </li>
-              </ul>
-            </nav>
-          </Header>
-
-          <div className="film-card__poster film-card__poster--small">
-            <img src={filmData.posterImage} alt={filmData.name} width="218" height="327"/>
-          </div>
-        </div>
+        )}
 
         <ReviewForm />
 
